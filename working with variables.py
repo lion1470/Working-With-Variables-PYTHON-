bdff4a67521b15a91bf2047e250146c917cc7b66
Ethan <73604746+lion1470@users.noreply.github.com>
2020-12-06 17:10:22 +0530

    added new file

---
+++ working with variables.py
@@ -0,0 +1,21 @@
+#if a = 10 (Here a is working as tag and mamory value is for 10  )
+# Python mein value ke liye memory banta hai na ki variable ke liye
+#a variables allows us to store a value by assigning it to name, which can be used to refer to the value later in the program.
+#FOR EXAMPLE IN GAME DEVLOPEMENT, WE WOULD USE A VARIABLES TO STORE THE POINTS OF THE PLAYER.
+#TO asign a variables, use one equals(=) sign.
+character_name = "john"
+character_age = "54"
+print("there was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+anas = "coder"
+print(anas + ' abdullah ' *  2)
+foo = 4
+bar = 3
+del foo
+foo = 2
+print(foo * bar)
+print(foo + bar)
+print(foo - bar)
+print(foo / bar)
+#we can use the (del) staement to remove a variable, which means the reference
+# from the name to value is deleted, and trying to use the variable causes an errora
